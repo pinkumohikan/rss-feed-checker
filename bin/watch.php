e07e3f2a4464752820351c68112253988a7159af
Hokuto Shinoda <mohimovi@gmail.com>
2017-01-12 00:55:16 +0900

    feat(bin): 実行ファイルを用意 (wip)

---
+++ bin/watch.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once dirname(__DIR__).'/bootstrap.php';
+
+use App\RssFeed\ParserFactory;
+use App\UpdateNotify\Notifier\Mail;
+
+$xml = file_get_contents(getenv('ATOM_URL'));
+
+$parser = (new ParserFactory())->create('atom');
+$entries = $parser->parse($xml);
+$latestEntry = array_shift($entries);
+
+if ($latestEntry->isNotified()) {
+    print '最新のエントリーは通知済みでした';
+    return;
+}
+
+$latestEntry->markAsNotified();
+
+$message = "ブログを書きました。
+{$latestEntry->link}";
+
+// 抽象化層作る
+$notifier = new Mail();
+$notifier->notify(
+    getenv('MAIL_TO'),
+    '',
+    $message
+);
